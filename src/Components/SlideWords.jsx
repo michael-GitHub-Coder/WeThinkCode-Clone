3ea856ae648f3148a5fbe2d4714be682be0328cc
michael-GitHub-Coder <71131317+michael-GitHub-Coder@users.noreply.github.com>
2024-09-12 15:16:01 +0200

    w

---
+++ src/Components/SlideWords.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SlideWords = () => {
+  return (
+    <div>
+    <h1 className="text-2xl font-semibold text-stone-700">Join Managers Hiring Top Tech Talent</h1>
+    </div>
+  )
+}
+
+export default SlideWords
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import Scroll from '../Components/Scroll'
+import SlideWords from '../Components/SlideWords'
 
 const HomePage = () => {
   return (
     <div>
         <Hero />
         <Scroll />
+        <SlideWords />
     </div>
   )
 }
