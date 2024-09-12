faab8553ea740054371353ed2b0a507366d70ef1
michael-GitHub-Coder <71131317+michael-GitHub-Coder@users.noreply.github.com>
2024-09-12 14:59:20 +0200

    second commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,21 +1,44 @@
 import React from 'react'
-import { CiStar } from "react-icons/ci";
+import { IoIosStar } from "react-icons/io";
+
 
 const Contact = () => {
   return (
-    <div className="bg-gray-100">
-        <div className="grid grid-cols-1 md:grid-cols-2">
+    <div className="bg-gray-100 h-auto py-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 container mx-auto max-w-4xl gap-5">
             <div>
-              <h1>Developing the finest talent in Africa</h1>
+              <h1 className="font-semibold text-2xl py-4">Developing the finest talent in Africa</h1>
               <p>Your future in tech starts here. Apply now and pave the way for your journey toward success.</p>
 
-              <div >
-                <CiStar color="#f59e0b"/>
-                <CiStar />
-                <CiStar />
-                <CiStar />
-                <CiStar />
+              <div className="flex pt-4">
+              <IoIosStar color="#f59e0b"/>
+              <IoIosStar color="#f59e0b"/>
+              <IoIosStar color="#f59e0b"/>
+              <IoIosStar color="#f59e0b"/>
+              <IoIosStar color="#f59e0b"/>
               </div>
+              <p>Rated Excellent (285+ Reviews)</p>
+            </div>
+            {/* form */}
+
+            <div>
+              <form>
+                  <div className="flex gap-4 ">
+                    <input type="text" placeholder="Name" className="py-3 px-5" />
+                    <input type="text" placeholder="Surname" className="py-3 px-5"/>
+                  </div>
+                  <div className="flex gap-4 py-2 ">  
+                    <input type="text" placeholder="Email Address" className="py-3 px-5"/>
+                    <input type="text" placeholder="Phone Number" className="py-3 px-5"/>
+                  </div>
+                  <select className="block px-3 py-2 border border-gray-300 focus:outline-none pt-2">
+                      <option value="" >What would you like to enquire about?</option>
+                      <option value="option1">General</option>
+                      <option value="option2">Students</option>
+                      <option value="option3">Marketing Enquiry</option>
+                      <option value="option3">Applications</option>
+                  </select>
+              </form>
             </div>
         </div>
     </div>
