c832fda75713a0c9b935e7c1683ef918380a35b2
michael-GitHub-Coder <71131317+michael-GitHub-Coder@users.noreply.github.com>
2024-09-13 10:50:10 +0200

    w

--- src/Components/Gray.jsx
+++ src/Components/Gray.jsx
@@ -63,11 +63,20 @@ const Gray = () => {
                 <h1 className="font-stone-700 font-semibold">WeThinkCode_</h1>
                 <h1 onClick={()=> setUpDown(!UpDown)} className="flex gap-2">{links.length} Videos {UpDown ?  <BiSolidUpArrow className="md:hidden mt-1.5"/> : <BiSolidDownArrow className="md:hidden mt-1.5"/>}</h1>
             </div>
-            <div>
+            <div className="hidden md:block">
+                {links.map((links) => (
+                    <div className="flex gap-6">
+                        <h1 className="text-sm py-2 text-red-400 hover:bg-gray-100 hover:text-gray-400">{links.title.substring(0,25)}</h1>
+                        <h1 className="mt-2">{links.duration}</h1>
+                    </div>
+                ))}
+            </div>
+            <div className="md:hidden">
                 {links.map((links) => (
                     UpDown && 
-                    <div>
-                        <h1 className="text-sm py-2 text-red-400 hover:bg-gray-100 hover:text-gray-400">{links.title}</h1>
+                    <div className="flex gap-6">
+                        <h1 className="text-sm py-2 text-red-400 hover:bg-gray-100 hover:text-gray-400">{links.title.substring(0,25)}</h1>
+                        <h1 className="mt-2">{links.duration}</h1>
                     </div>
                 ))}
             </div>
