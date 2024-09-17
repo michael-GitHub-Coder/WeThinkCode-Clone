2a4cf5a4c9fec3a70723e63504f2211199ac3df2
michael-GitHub-Coder <71131317+michael-GitHub-Coder@users.noreply.github.com>
2024-09-17 14:53:26 +0200

    w

--- src/Pages/PartnersPage.jsx
+++ src/Pages/PartnersPage.jsx
@@ -9,8 +9,11 @@ const PartnersPage = () => {
             <p className="py-10 text-gray-500">Collaborate with WeThinkCode_ by investing in securing a pipeline of diverse, work-ready, entry-level software developers; by funding sustainable social impact outcomes; imparting your knowledge and experience through our mentorship and work readiness programmes.</p>
         </div>
         <div className="grid grid-cols-1 md:flex gap-4 pb-4 container mx-auto max-w-4xl">
-            <div className="md:h-72 md:w-72 w-full">
+            <div className="md:h-72 md:w-72 w-full relative">
                 <img src="https://wethinkcode.co.za/wp-content/uploads/2024/03/Philani-Ngcamu_Glass.webp"/>
+                <div className="absolute bottom-0 left-0">
+                    <button className="bg-green-600 text-white font-semibold py-2 px-4">Sponsor Learners</button>
+                </div>
             </div>
             <div className="md:h-72 md:w-72 w-full">
                 <img src="https://wethinkcode.co.za/wp-content/uploads/2024/03/Philani-Ngcamu_Glass.webp"/>
