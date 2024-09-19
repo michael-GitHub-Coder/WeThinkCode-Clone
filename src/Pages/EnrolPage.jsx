5c685f8b75d0405a5306f4a46cb9b394a74b81d9
michael-GitHub-Coder <71131317+michael-GitHub-Coder@users.noreply.github.com>
2024-09-19 13:27:36 +0200

    w

--- src/Pages/EnrolPage.jsx
+++ src/Pages/EnrolPage.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
 import Header from '../Components/Header'
+import { useState } from 'react'
+import { BiSolidDownArrow, BiSolidUpArrow } from "react-icons/bi";
 
 const EnrolPage = () => {
+
+  const [isExpanded, setIsExpanded] = useState(true);
+  const [currentVideoUrl, setCurrentVideoUrl] = useState("https://youtu.be/8Xgf7mOnRb4");
+
+  const videos = [
+      {
+          url: "https://youtu.be/8Xgf7mOnRb4",
+          title: "#HireTheFuture from WeThinkCode_",
+          duration: "1:13",
+      },
+      {
+          url: "https://youtu.be/aV0h6ZR8zyU",
+          title: "System Verification & WeThinkCode Podcast: Test Automation & Quality Assurance",
+          duration: "41:13",
+      },
+      {
+          url: "https://youtu.be/S47c0Qos-eQ?t=4",
+          title: "WeThinkCode_ Attains NQF 6 Occupational Certificate Accredited by Q",
+          duration: "0:24",
+      },
+      {
+          url: "https://youtu.be/zCT7PlTNSoc?t=1",
+          title: "Getting more women into tech",
+          duration: "6:23",
+      }
+  ];
+  const getVideoId = (url) => {
+      const urlObj = new URL(url);
+      if (urlObj.hostname === 'youtu.be') {
+          return urlObj.pathname.substring(1);
+      }
+      return urlObj.searchParams.get('v');
+  };
+
   return (
     <div>
       <Header image={"https://wethinkcode.co.za/wp-content/uploads/2024/03/Enrol-header.webp"} title="Kickstart your career in tech"  pos="500% 403%" />
@@ -15,7 +51,47 @@ const EnrolPage = () => {
         </div>
       </div>
 
-      
+      <div className="container mx-auto max-w-4xl grid grid-cols-1 md:flex gap-3 mt-10 ">
+                <div className="md:col-span-2 ">
+                    {/* TODO: */}
+                    <iframe
+                        width="100%"
+                        height="340"
+                        src={`https://www.youtube.com/embed/${getVideoId(currentVideoUrl)}`}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        className="md:w-[600px]"
+                    />
+
+                </div>
+                {/* Titles */}
+                <div>
+                    <div className="flex gap-20">
+                        <h1 className="font-stone-700 font-semibold">WeThinkCode_</h1>
+                        <h1 onClick={() => setIsExpanded(!isExpanded)} className="flex gap-2">
+                            {videos.length} Videos
+                            {isExpanded ? <BiSolidUpArrow className="md:hidden mt-1.5" /> : <BiSolidDownArrow className="md:hidden mt-1.5" />}
+                        </h1>
+                    </div>
+                    <div className="hidden md:block">
+                        {videos.map((video) => (
+                            <div key={video.url} onClick={() => setCurrentVideoUrl(video.url)} className="flex gap-6 hover:bg-gray-100 cursor-pointer">
+                                <h1 className="text-sm py-2 text-red-400 hover:text-gray-400">{video.title.substring(0, 25)}</h1>
+                                <h1 className="mt-2">{video.duration}</h1>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="md:hidden">
+                        {isExpanded && videos.map((video) => (
+                            <div key={video.url} onClick={() => setCurrentVideoUrl(video.url)} className="flex gap-6 hover:bg-gray-100 cursor-pointer">
+                                <h1 className="text-sm py-2 text-red-400 hover:text-gray-400">{video.title.substring(0, 25)}</h1>
+                                <h1 className="mt-2">{video.duration}</h1>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
     </div>
   )
 }
