eab3993327f4f114a3280f8d0a56a942f82da271
michael-GitHub-Coder <71131317+michael-GitHub-Coder@users.noreply.github.com>
2024-09-19 13:10:14 +0200

    news

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const News = ({image,title,para}) => {
+const News = ({ image, title, para }) => {
   return (
-    <div className="bg-white shadow-lg h-auto w-72 mt-40 rounded">
-        <div className="h-[50%]">
-            <img src={image} alt="newIMG" />
-        </div>
-       <div className="p-5">
-            <h1 className="text-blue-500 text-md font-semibold">{title}</h1>
-            <p className="text-xs text-gray-400 py-3">{para}</p>
-            <button className="text-blue-500">Read More »</button>
-       </div>
+    <div className="bg-white shadow-lg h-[450px] w-72 rounded mt-5 pb-5">
+      <div className="relative h-1/2">
+        <img
+          src={image}
+          alt="newIMG"
+          className="absolute inset-0 w-full h-full object-cover rounded-t-md"
+        />
+      </div>
+      <div className="px-5 py-2 ">
+        <h1 className="text-blue-500 text-md font-semibold">{title}</h1>
+        <p className="text-xs text-gray-400 py-3">{para}</p>
+        <button className="text-blue-500">Read More »</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
--- src/Pages/NewsPage.jsx
+++ src/Pages/NewsPage.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import News from '../Components/News'
+import data from "../News.json"
+
 
 const NewsPage = () => {
+
   return (
     <div>
-      {}
-      <News image="https://wethinkcode.co.za/wp-content/uploads/2024/09/CAPE-TOWN-CAMPUS.jpg.webp" title="WeThinkCode_ breaks graduate record" para="WeThinkCode_ breaks graduate record with Class of 2024 Software development academy WeThinkCode_ has celebrated its largest graduating"/>
+      <h1 className="text-center font-bold text-stone-700 mt-36 text-3xl md:text-4xl pb-2">WeThinkCode_ News</h1>
+      <div className="container mx-auto max-w-4xl grid grid-cols-1 md:grid-cols-3">
+       
+        { data.News.map((daa) => (
+            <div className="flex justify-center">
+              <News image={daa.url} title={daa.title} para={daa.para.substring(0,150)+"..."}/>
+            </div>
+        ))}
+    </div>
     </div>
   )
 }
